2dc86d49315e1385f3494c1925e921c8dda73cdc
aaljo222 <125717229+aaljo222@users.noreply.github.com>
2024-12-31 09:02:54 +0900

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,20 +1,20 @@
-from flask import Flask, request
-from flask_cors import CORS
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
-CORS(app)  # 모든 출처에서의 접근을 허용
 
 @app.route('/api/proxy', methods=['GET'])
 def proxy():
-    target_url = request.args.get('url')
-    if not target_url:
-        return {'error': 'Missing target URL'}, 400
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL parameter is required"}), 400
+
     try:
-        response = requests.get(target_url)
-        return response.content, response.status_code, {'Content-Type': response.headers['Content-Type']}
-    except Exception as e:
-        return {'error': str(e)}, 500
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
+        return response.text, response.status_code
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
